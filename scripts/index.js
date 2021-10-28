ba8cebb4a6fe6496399570960a1d0e269cccb142
Junior Santos <gilsonsantosjunior02@gmail.com>
2021-10-28 18:09:26 -0300

    adicionando pagina nova

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// botao menu 
+var sairmenu = window.document.getElementById('list_menu')
+sairmenu.style.display = 'none'
+
+function click_menu() {
+    var click_button = window.document.getElementById('list_menu')
+    click_button.style.display = 'block'
+}
+
+function sair_menu() {
+   sairmenu.style.display = 'none'
+}
