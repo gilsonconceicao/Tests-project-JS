eb1c22ef281d135d5a9dc30f154f7f4eabc3673c
Junior Santos <gilsonsantosjunior02@gmail.com>
2021-10-29 17:38:17 -0300

    commit

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -17,3 +17,11 @@ function dividir() {
     
     res_divisao.innerHTML = `${dividirN1} / ${dividirN2} = ${valor_divisao}`
 }
+
+function somar() {
+    var maisN1 = Number(inputMaisN1.value)
+    var maisN2 = Number(inputMaisN2.value)
+    var valor_somar = maisN1 + maisN2
+    var res_somar = window.document.getElementById('res_mais')
+    res_somar.innerHTML = `${maisN1} + ${maisN2} = ${valor_somar}`
+}
